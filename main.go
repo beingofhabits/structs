abffab861d5ea2ecaf6efa13902e12f85d6d3764
beingofhabits <beingofhabits@gmail.com>
2018-11-11 22:33:21 +0300

    break

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}
+	var alex person
+
+	fmt.Printf("%+v", alex)
 }
